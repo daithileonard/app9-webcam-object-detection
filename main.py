6d3dd98d2fd398f7794cb4f0824627dc0bc8d9fe
daithileonard <DAITHILEONARD@GMAIL.COM>
2023-05-04 11:55:47 +0100

    added video capture, grayscaling, gaussing and dilation.

--- main.py
+++ main.py
@@ -4,8 +4,29 @@ import time
 video = cv2.VideoCapture(0)
 time.sleep(1)
 
+first_frame = None
+
 while True:
     check, frame = video.read()
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0.1)
+    cv2.imshow("My Vid", gray_frame_gau)
+
+    if first_frame is None:
+        first_frame = gray_frame_gau
+    delta_frame = cv2.absdiff(first_frame, gray_frame_gau)
+
+    thresh_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
+    dil_frame = cv2.dilate(thresh_frame, None, iterations=1)
+    cv2.imshow("My Vid", dil_frame)
+
+    contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        if cv2.contourArea(contour) < 4500:
+            continue
+        x, y, w, h = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
     cv2.imshow("My Vid", frame)
 
     key = cv2.waitKey(1)
@@ -13,7 +34,6 @@ while True:
         break
 
 video.release()
-
 
 print(check)
 print(frame)
