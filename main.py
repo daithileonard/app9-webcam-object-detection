a27c3a6b259d6eef6674dcc12c628688fc2476c0
daithileonard <DAITHILEONARD@GMAIL.COM>
2023-05-08 14:54:08 +0100

    Add emailing.py detail

--- main.py
+++ main.py
@@ -38,12 +38,14 @@ while True:
             cv2.imwrite(f"images/{count}_image.png", frame)
             count = count + 1
             all_images = glob.glob("images/*.png")
+            index = int(len(all_images) / 2)
+            image_with_object = all_images[index]
 
     status_list.append(status)
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        send_email(image_with_object)
 
     cv2.imshow("My Vid", frame)
 
